cca9c4a7b6735889e4c747e9eab1488a2875488c
Matthew Delgado <mdelgado@up-rev.com>
2023-12-03 21:01:17 -0500

    created simple script to simulate flipping a coin

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,15 @@
-def func(x):
-    return x + 1 and print(x + 1)
+import random
 
+def flip_coin():
+    # Generate a random number (0 or 1) to represent heads or tails
+    result = random.randint(0, 1)
+    
+    # Use the result to determine the outcome
+    if result == 0:
+        return "Heads"
+    else:
+        return "Tails"
 
-def test_answer():
-    assert func(3) == 5
+# Call the flip_coin function and print the result
+outcome = flip_coin()
+print("The coin landed on:", outcome)
