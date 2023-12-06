1772f179f57b1d1f113ebb9201940a73a214039e
Matthew Delgado <mdelgado@up-rev.com>
2023-12-06 10:24:15 -0500

    removed comments

--- coin_flip.py
+++ coin_flip.py
@@ -4,12 +4,11 @@ def coin_flip():
     # Generate a random number (0 or 1) to represent heads or tails
     result = random.randint(0, 1)
     
-    # Use the result to determine the outcome
     if result == 0:
         return "Heads"
     else:
         return "Tails"
 
-# Call the flip_coin function and print the result
 outcome = coin_flip()
+
 print(outcome)
