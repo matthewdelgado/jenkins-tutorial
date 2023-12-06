2711dc81bda4777202f5abcea660f2f9963445c3
Matthew Delgado <mdelgado@up-rev.com>
2023-12-06 14:30:02 -0500

    changes

--- test_not_null.py
+++ test_not_null.py
@@ -1,4 +1,4 @@
 from coin_flip import coin_flip
 
 def test_coin_flip():
-    assert coin_flip() is not None, "Result should not be None"
+    assert coin_flip() is None, "Result should not be None"
